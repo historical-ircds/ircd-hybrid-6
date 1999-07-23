4aba11e4b7fb426f6a3e89b091ee1600cb464313
Diane Bruce <db@db.net>
1999-07-23 13:33:06 +0000

    - last bit of fixes

--- include/list.h
+++ include/list.h
@@ -27,6 +27,11 @@
 #ifndef INCLUDED_list_h
 #define INCLUDED_list_h
 
+struct SLink;
+struct Client;
+struct Class;
+struct User;
+
 extern void count_user_memory(int *, int *);
 extern void count_links_memory(int *, int *);
 extern void count_flud_memory(int *, int *);
