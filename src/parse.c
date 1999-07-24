bce9ab307af1441a913f94454f6435ea1274f947
Thomas Helvey <tomh@inxpress.net>
1999-07-24 04:08:42 +0000

    cleanup compile warning: parse.c:183: warning: numeric might be used uninitialized in this function

--- src/parse.c
+++ src/parse.c
@@ -174,13 +174,13 @@ aClient *find_person(char *name, aClient *cptr)
  *
  * NOTE: parse() should not be called recusively by any other functions!
  */
-int     parse(aClient *cptr, char *buffer, char *bufend)
+int parse(aClient *cptr, char *buffer, char *bufend)
 {
   aClient *from = cptr;
   char  *ch;
   char  *s;
   int   i;
-  char  *numeric;
+  char* numeric = 0;
   int   paramcount;
   struct Message *mptr;
 
