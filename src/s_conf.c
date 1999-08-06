0d500dac1cfe8afd45afb432bf83393ece231a30
Jon Lusky <lusky@blown.net>
1999-08-06 04:26:26 +0000

    fixed rehash dump, would core on bad admin line

--- src/s_conf.c
+++ src/s_conf.c
@@ -1635,11 +1635,20 @@ int rehash_dump(aClient *sptr)
   char result_buf[256];
   char timebuffer[MAX_DATE_STRING];
   struct tm *tmptr;
+  char *host;
+  char *pass;
+  char *user;
+  char *name;
+  int  port;
 
   tmptr = localtime(&CurrentTime);
   strftime(timebuffer, MAX_DATE_STRING, "%Y%m%d%H%M", tmptr);
-  (void)sprintf(ircd_dump_file,"%s/ircd.conf.%s",
-                DPATH,timebuffer);
+  if (DPATH[strlen(DPATH)-1] == '/')
+     (void)sprintf(ircd_dump_file,"%sircd.conf.%s",
+                   DPATH,timebuffer);
+  else
+     (void)sprintf(ircd_dump_file,"%s/ircd.conf.%s", 
+		   DPATH, timebuffer);
   
   if ((out = fbopen(ircd_dump_file, "a")) == 0)
     {
@@ -1654,46 +1663,47 @@ int rehash_dump(aClient *sptr)
   for(aconf = ConfigItemList; aconf; aconf = aconf->next)
     {
       aClass* class_ptr = ClassPtr(aconf);
+      get_printable_conf(aconf, &name, &host, &pass, &user, & port );
 
       if(aconf->status == CONF_CONNECT_SERVER)
         {
           
           (void)sprintf(result_buf,"C:%s:%s:%s::%d\n",
-                        aconf->host,aconf->passwd,
-                        aconf->name,
+                        host,pass,
+                        name,
                         ClassType(class_ptr));
           fbputs(result_buf, out);
         }
       else if(aconf->status == CONF_NOCONNECT_SERVER)
         {
           (void)sprintf(result_buf,"N:%s:%s:%s::%d\n",
-                        aconf->host,aconf->passwd,
-                        aconf->name,
+                        host,pass,
+                        name,
                         ClassType(class_ptr));
           fbputs(result_buf, out);
         }
       else if(aconf->status == CONF_OPERATOR)
         {
           (void)sprintf(result_buf,"O:%s@%s:%s:%s::%d\n",
-                        aconf->user,aconf->host,
-                        aconf->passwd,
-                        aconf->name,
+                        user,host,
+                        pass,
+                        name,
                         ClassType(class_ptr));
           fbputs(result_buf, out);
         }
       else if(aconf->status == CONF_LOCOP)
         {
           (void)sprintf(result_buf,"o:%s@%s:%s:%s::%d\n",
-                        aconf->user,aconf->host,
-                        aconf->passwd,
-                        aconf->name,
+                        user,host,
+                        pass,
+                        name,
                         ClassType(class_ptr));
           fbputs(result_buf, out);
         }
       else if(aconf->status == CONF_ADMIN)
         {
           (void)sprintf(result_buf,"A:%s:%s:%s::\n",
-                        aconf->user,aconf->passwd,aconf->name);
+                        host,pass,user);
           fbputs(result_buf, out);
         }
     }
