28b684f5388320b6ee1926ae449fec5f89401256
Lee Hardy <leeh@leeh.co.uk>
2001-06-06 13:38:47 +0000

    - heres another one :P

--- src/m_info.c
+++ src/m_info.c
@@ -34,6 +34,8 @@ m_info(aClient *cptr, aClient *sptr, int parc, char *parv[])
   static time_t last_used=0L;
   Info *infoptr;
 
+  if(IsServer(sptr))
+    return 0;
   if (hunt_server(cptr,sptr,":%s INFO :%s",1,parc,parv) == HUNTED_ISME)
   {
     sendto_realops_flags(FLAGS_SPY, "info requested by %s (%s@%s) [%s]",
